8465b59b16b494fb3b2fd5ff6b87d5cf47369377
EkinBear <ekinbasarkomur@hotmail.com>
2024-07-23 23:35:15 +0300

    working demo

--- cyberclops.py
+++ cyberclops.py
@@ -5,136 +5,62 @@ import pytesseract
 from PIL import Image
 import pyperclip
 
-#pyperclip.copy('text to be copied')
+
 min_conf = 80
+
+start_point = None
+end_point = None
+drawing = False
+
+
 
 pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract'
 
-def available_cameras():
-    index = 0
-    arr = []
-    while True:
-        cap = cv2.VideoCapture(index)
-        if not cap.read()[0]:
-            break
-        else:
-            arr.append(index)
-        cap.release()
-        index += 1
 
-    return arr
+def overlay_image(image, overlay, position):
+    """Overlay the resized image onto the background at the specified position."""
+    x, y = position
+    h, w = overlay.shape[:2]
 
+    # Ensure the overlay image fits within the background
+    if y + h > image.shape[0]:
+        h = image.shape[0] - y
+    if x + w > image.shape[1]:
+        w = image.shape[1] - x
 
-def detect_text(image):
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = pytesseract.image_to_data(rgb, output_type = pytesseract.Output.DICT, lang = "tur")
-    #text = pytesseract.image_to_string(Image.fromarray(img1))
-    #print(text)
-
-
-    for i in range(0, len(results["text"])): 
-        # We can then extract the bounding box coordinates 
-        # of the text region from  the current result 
-        x = results["left"][i] 
-        y = results["top"][i] 
-        w = results["width"][i] 
-        h = results["height"][i] 
-        
-        # We will also extract the OCR text itself along 
-        # with the confidence of the text localization 
-        text = results["text"][i] 
-        conf = int(results["conf"][i]) 
-        
-        # filter out weak confidence text localizations 
-        if conf > min_conf: 
-            
-            # We will display the confidence and text to 
-            # our terminal 
-            print("Confidence: {}".format(conf)) 
-            print("Text: {}".format(text)) 
-            print("") 
-            
-            # We then strip out non-ASCII text so we can 
-            # draw the text on the image We will be using 
-            # OpenCV, then draw a bounding box around the 
-            # text along with the text itself 
-            text = "".join(text).strip() 
-            cv2.rectangle(image, 
-                        (x, y), 
-                        (x + w, y + h), 
-                        (0, 0, 255), 2) 
-            cv2.putText(image, 
-                        text, 
-                        (x, y - 10),  
-                        cv2.FONT_HERSHEY_SIMPLEX, 
-                        1.2, (0, 255, 255), 3) 
+    # Place the overlay image on the background
+    image[y:y+h, x:x+w] = overlay[:h, :w]
 
     return image
 
 
 
-def mark_region(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (7,7), 0)
-    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-    # Create rectangular structuring element and dilate
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
-    dilate = cv2.dilate(thresh, kernel, iterations=5)
-
-    # Find contours and draw rectangle
-    cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    for c in cnts:
-        x,y,w,h = cv2.boundingRect(c)
-        cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
-
-    return image
 
 
-def strech_image(image):
+def select_region(event, x, y, flags, param):
+    global start_point, end_point, drawing
 
-    # Mask label
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (7,7), 0)
-    thresh = cv2.threshold(blur, 120, 255, cv2.THRESH_BINARY)[1]
-    edged = cv2.Canny(blur, 30, 150)
+    if event == cv2.EVENT_LBUTTONDOWN:
+        # Start drawing the rectangle
+        drawing = not drawing
+        if drawing:
+            start_point = (x, y)
 
-    # Find contours
-    cnts = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    c = max(cnts, key=cv2.contourArea)
-
-    # Obtain outer coordinates
-    left = tuple(c[c[:, :, 0].argmin()][0])
-    right = tuple(c[c[:, :, 0].argmax()][0])
-    top = tuple(c[c[:, :, 1].argmin()][0])
-    bottom = tuple(c[c[:, :, 1].argmax()][0])
-
-    cv2.drawContours(image, [c], -1, (36, 255, 12), 2)
-
-    cv2.circle(image, left, 8, (0, 50, 255), -1)
-    cv2.circle(image, right, 8, (0, 255, 255), -1)
-    cv2.circle(image, top, 8, (255, 50, 0), -1)
-    cv2.circle(image, bottom, 8, (255, 255, 0), -1)
-
-
-    bw, bh = [400, 200]
-    pts1 = np.float32([top, left, bottom, right])
-    pts2 = np.float32([[0, 0], [0, bh-1], [bw-1, bh-1], [bw-1, 0]])
-    M = cv2.getPerspectiveTransform(pts1, pts2)
-    warped = cv2.warpPerspective(image, M, (bw, bh))
-
-    warped = image[top[1]:bottom[1], left[0]:right[0]]
-    
-    return image
-
-
+    if event == cv2.EVENT_MOUSEMOVE:
+        if drawing:
+            # Update the end point as the mouse moves
+            end_point = (x, y)
 
 
 def main():
     # create display window
     cv2.namedWindow("Cyberclops", cv2.WINDOW_NORMAL)
+    cv2.setMouseCallback("Cyberclops", select_region)
+
+    cropped_image = None
+    cropped = False
+
+
     
     #print(available_cameras())
 
@@ -156,7 +82,12 @@ def main():
 
     # main loop: retrieves and displays a frame from the camera
     while (True):
-        success, img = cap.read()
+        success, frame = cap.read()
+        clone = frame.copy()
+
+
+        if not success:
+            break
 
 
         # FPS Calculation
@@ -164,23 +95,66 @@ def main():
         delta_time = datetime.datetime.now() - last_time
         elapsed_time = delta_time.total_seconds()
         cur_fps = np.around(frames / elapsed_time, 1)
-        cv2.putText(img, 'FPS: ' + str(cur_fps), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(clone, 'FPS: ' + str(cur_fps), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-        #img = detect_text(img)
 
-        #img = mark_region(img)
 
-        img = strech_image(img)
+        if start_point and end_point:
+            # Draw the rectangle on the frame
+            cv2.rectangle(clone, start_point, end_point, (0, 255, 0), 2)
 
+            if not drawing and not cropped:
+                x1, y1 = start_point
+                x2, y2 = end_point
+
+                x1, x2 = sorted([x1, x2])
+                y1, y2 = sorted([y1, y2])
+            
+                # Extract and display the selected area from the last frame
+                cropped_image = frame[y1:y2, x1:x2]
+
+                ret,thresh1 = cv2.threshold(cropped_image,120,255,cv2.THRESH_BINARY)
+                text = str(pytesseract.image_to_string(thresh1, config='--psm 6', lang='tur'))
+
+                text = text.replace('\n', ' ')
+                text = text.replace('-\n', '')
+
+                pyperclip.copy(text)
+                print(text)
+                
+                #cropped_image = detect_text(cropped_image)
+                cropped = True
+            
+            elif drawing:
+                cropped = False
+                
+
+
+        if cropped_image is not None:
+            # Resize the overlay image (optional)
+            overlay_height = int(cap_height / 5) # Desired height of the overlay image
+            overlay_width = int(cropped_image.shape[1] * (overlay_height / cropped_image.shape[0]))
+            
+            resized_overlay = cv2.resize(cropped_image, (overlay_width, overlay_height))
+
+            # Position for the overlay image (bottom-right corner)
+            position = (frame.shape[0] - resized_overlay.shape[0], 
+                        0)
+            
+            """Overlay the resized image onto the background at the specified position."""
+            x, y = position
+            h, w = resized_overlay.shape[:2]
+
+            clone[x:x+h, y:y+w] = resized_overlay[:h, :w]
 
 
         # Display the frame
-        cv2.imshow("Cyberclops", img)
+        cv2.imshow("Cyberclops", clone)
 
 
-        # wait 1ms for ESC to be pressed
-        key = cv2.waitKey(1)
-        if (key == 27):
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            # Exit the loop when 'q' is pressed
             break
 
     # release resources
